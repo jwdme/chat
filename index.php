34988d88a721c298a9107d2e19da73f11bd18259
Jared De Blander <jared@deblander.org>
2014-05-31 17:04:06 -0400

    routes and templates

--- index.php
+++ index.php
@@ -2,7 +2,20 @@
 
 require "vendor/autoload.php";
 
+define('TWIG_TEMPLATE_PATH',        'templates/');
+define('TWIG_TEMPLATE_CACHE_PATH',  false);
 
+$loader = new Twig_Loader_Filesystem(TWIG_TEMPLATE_PATH);
+$twig = new Twig_Environment(
+    $loader,
+    array(
+        # Define the path to our templates
+        'cache' => TWIG_TEMPLATE_CACHE_PATH,
+
+        # Use new templates as soon as they are updated
+        'auto_reload' => true
+    )
+);
 
 $app = new \Slim\Slim();
 
@@ -14,5 +27,12 @@ $app->get('/about' ,function () {
     echo "<pre>Written by Jared De Blander\n\n";
     passthru('git branch -v');
 });
+$app->get('/:' , function ($name) {
+    echo $twig->render($template, (''));
+});
+$app->post('/create' , function () {
+    echo '<pre>';
+    var_dump($_POST);
+})
 $app->run();
 
