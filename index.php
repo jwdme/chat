ae0c25d86e0fbe50a05ffa1431d1294b87c6e579
Jared De Blander <jared@deblander.org>
2014-05-31 16:44:57 -0400

    adding routes again

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
 require "vendor/autoload.php";
-require 'templates/base.html';
+
+
+
+$app = new \Slim\Slim();
+
+
+$app->get('/', function () {
+    require 'templates/base.html';
+});
+$app->get('/about' ,function () {
+    echo "<pre>Written by Jared De Blander\n\n";
+    passthru('git branch -v');
+});
+$app->run();
 
