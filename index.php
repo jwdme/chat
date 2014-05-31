25663c4374f9a0679caf0a642720bca6c3f8c1b5
Jared De Blander <jared@deblander.org>
2014-05-31 17:45:36 -0400

    random rooms and passwords

--- index.php
+++ index.php
@@ -30,14 +30,33 @@ function view_landing () {
 }
 
 function view_about () {
-    echo 'Written by Jared De Blander';
+    twig_render('about.html' , array());
 }
 
 function view_chatroom ($name) {
     twig_render('chatroom.html' , array('roomname' => $name));
 }
 
+function join_random () {
+
+    header('Location: /' . random_string(12));
+}
+
 function create_room () {
+    trim_post_data();
+    if(!i sset($_POST['room']) || ! isset($_POST['password'])) {
+        die('please complte the form!')
+
+    } else {
+        $pass = $_POST['password'];
+        $room = $_POST['room'];
+        if(strlen($pass) === 0){
+            # No password sent to a room
+            header('Location: /' . $room);
+        } else {
+            header('Location: /' . sha1($room . $pass);
+        }
+    }
     echo '<pre>';
     var_dump($_POST);
 }
@@ -47,11 +66,29 @@ Support functions
 
 */
 
+function random_string ($length = 10) {
+    $str_out = '';
+    for($i = 0; $i < $length; $i++) {
+        $str_out .= substr(
+            str_shuffle(
+                'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+            ),
+            0,
+            1
+        );
+    }
+    return $str_out;
+}
 function twig_render($template, $vars) {
     global $twig;
     echo $twig->render($template, $vars);
 }
 
+function trim_post_data () {
+    foreach($_POST as $key=>$value){
+        $_POST[$key] = trim($value);
+    }
+}
 /*
 
 Routes
@@ -60,6 +97,7 @@ Routes
 
 $app->get('/',              'view_landing');
 $app->get('/about',         'view_about');
+$app->get('/random',        'join_random');
 $app->get('/:name',         'view_chatroom');
 $app->post('/create',       'create_room');
 $app->run();
